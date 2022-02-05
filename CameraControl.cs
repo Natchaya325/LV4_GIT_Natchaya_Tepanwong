3a79895f7d6aa228f4983b329a45504e170ed9d1
Natchaya325 <snattncy@gmail.com>
2022-02-05 14:59:13 +0700

    first commit

---
+++ CameraControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Vector3 offset;
+    public GameObject ball;
+    void Start()
+    {
+        offset = transform.position = ball.transform.position; 
+    }
+    void Update()
+    {
+        transform.position = ball.transform.position+offset;  
+    }
+}
